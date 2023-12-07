f82f19ae089619c674572525ab7fac43066f8dc9
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-12-07 13:02:06 -0500

    Schedule a Call Page converted into react spa

---
+++ src/components/custom-switch/custom-switch.component.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import "./custom-switch.styles.css";
+
+const CustomSwitch = ({ toggleChecked, labelText, toggleStatus }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      toggleChecked(!toggleStatus);
+    }
+  };
+
+  return (
+    <div
+      role="switch"
+      aria-checked={toggleStatus}
+      tabIndex="0"
+      onClick={() => toggleChecked(!toggleStatus)}
+      onKeyDown={handleKeyDown}
+    >
+      <span className="switch">
+        <span></span>
+      </span>
+      <span className="on" aria-hidden="true"></span>
+      <span className="off" aria-hidden="true"></span>
+      <span className="label">{labelText}</span>
+    </div>
+  );
+};
+
+export default CustomSwitch;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/homepage/home.page.jsx";
 import Services from "./pages/services/services.page.jsx";
+import ScheduleCall from "./pages/schedule-a-call/schedule-call.page.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/services" Component={Services} />
+        <Route path="/schedule-call" Component={ScheduleCall} />
       </Routes>
       <footer className="container">
         <p>© Copyright Accessible Knowledge Base 2023</p>
